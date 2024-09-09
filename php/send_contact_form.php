e5038d79ae95c5b53f65d34c04784f4f5a7a6a2f
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2024-09-09 19:06:18 +0200

    Retest database writening.

--- php/send_contact_form.php
+++ php/send_contact_form.php
@@ -1,5 +1,4 @@
 <?php
-// Adatbázis kapcsolati beállítások
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -13,22 +12,20 @@ if ($conn->connect_error) {
     die("Kapcsolat hiba: " . $conn->connect_error);
 }
 
-// Az űrlap adatai
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+// Adatok ellenőrzése és mentése
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
 
 // SQL lekérdezés
-$sql = "INSERT INTO contact_form (name, email, phone, message)
-        VALUES ('$name', '$email', '$phone', '$message')";
+$sql = "INSERT INTO contact_form (name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Az üzenet sikeresen elküldve!";
+    echo "Üzenet elküldve!";
 } else {
     echo "Hiba: " . $sql . "<br>" . $conn->error;
 }
 
-// Kapcsolat bezárása
 $conn->close();
 ?>
