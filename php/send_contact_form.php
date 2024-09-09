7c1da37eafd6cbd95606a7328a6bb283740d3367
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2024-09-09 18:54:43 +0200

    Retest database communication

--- php/send_contact_form.php
+++ php/send_contact_form.php
@@ -1,35 +1,32 @@
 <?php
-// Adatbázis kapcsolat beállításai
+// Adatbázis kapcsolati beállítások
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "contact_form";
+$dbname = "szabaduloszoba";
 
-// Kapcsolódás az adatbázishoz
+// Kapcsolat létrehozása
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Kapcsolat ellenőrzése
 if ($conn->connect_error) {
-    die("Kapcsolódási hiba: " . $conn->connect_error);
+    die("Kapcsolat hiba: " . $conn->connect_error);
 }
 
-// Adatok fogadása az űrlapból
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-    $consent = isset($_POST['consent']) ? 1 : 0;
+// Az űrlap adatai
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-    // SQL lekérdezés beszúrásra
-    $sql = "INSERT INTO contacts (name, email, phone, message, consent) 
-            VALUES ('$name', '$email', '$phone', '$message', '$consent')";
+// SQL lekérdezés
+$sql = "INSERT INTO contact_form (name, email, phone, message)
+        VALUES ('$name', '$email', '$phone', '$message')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Az üzenet sikeresen elküldve.";
-    } else {
-        echo "Hiba történt: " . $sql . "<br>" . $conn->error;
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "Az üzenet sikeresen elküldve!";
+} else {
+    echo "Hiba: " . $sql . "<br>" . $conn->error;
 }
 
 // Kapcsolat bezárása
